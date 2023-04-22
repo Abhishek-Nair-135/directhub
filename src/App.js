761dd38927021eabea855493ca75d96313f71027
Abhishek Nair <abhishek@instoried.com>
2023-04-22 10:40:25 +0530

    Routes declared

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/Dashboard";
-import PullRequests from "./pages/PullRequests";
-import Issues from "./pages/Issues";
-import Details from "./pages/Details";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import PullRequests from "./pages/PullRequests/PullRequests";
+import Issues from "./pages/Issues/Issues";
+import Details from "./components/Details/Details";
+import IssueDetails from "./pages/IssueDetails/IssueDetails";
+import PullRequestDetails from "./pages/PullRequestDetails/PullRequestDetails";
 
 function App() {
     return (
@@ -11,12 +13,12 @@ function App() {
                 <Route path="/">
                     <Route index element={<Dashboard />} />
                     <Route path="issues">
-                        <Route index element={<Issues />} />
-                        <Route path=":owner/:repo/:number" element={<Details page="issues" />} />
+                        <Route path=":owner/:repo" element={<Issues />} />
+                        <Route path=":owner/:repo/:issue_number" element={<IssueDetails />} />
                     </Route>
                     <Route path="pulls">
-                        <Route index element={<PullRequests />} />
-                        <Route path=":owner/:repo/:number" element={<Details page="pulls" />} />
+                        <Route path=":owner/:repo" element={<PullRequests />} />
+                        <Route path=":owner/:repo/:pull_number" element={<PullRequestDetails />} />
                     </Route>
                 </Route>
             </Routes>
